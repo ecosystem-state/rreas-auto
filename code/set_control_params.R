1e1ca71ad9b939ee2bc161e9d818cfc23b09aadc
Eric Ward <eric.ward@noaa.gov>
2022-08-17 09:08:14 -0700

    trigger

--- code/set_control_params.R
+++ code/set_control_params.R
@@ -1,6 +1,7 @@
 # Parameters that apply to multiple scripts
 #min_year <- 1985
 pred_resolution <- 5 # resolution of prediction grid, km
+
 #top_species <- 30 # not used
 lat_max = 38.1884 # used to define core area
 lat_min = 36.544 # used to define core area
