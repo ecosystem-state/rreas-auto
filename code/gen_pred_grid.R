195a6421fd25d8e15ba08325cbbd94706f8cd4d0
Eric Ward <eric.ward@noaa.gov>
2022-08-18 05:51:26 -0700

    Update gen_pred_grid.R

--- code/gen_pred_grid.R
+++ code/gen_pred_grid.R
@@ -16,25 +16,6 @@ dat = dplyr::filter(dat,
                     sci_name == spp)
 dat = dplyr::filter(dat, lat_dd < lat_max, lat_dd > lat_min)
 
-# coordinates(dat) <- c("longitude", "latitude")
-# proj4string(dat) <- CRS("+proj=longlat +datum=WGS84")
-# dat <- spTransform(dat, CRS("+proj=utm +zone=10"))
-# dat <- as.data.frame(dat)
-# dat$longitude <- dat$longitude/1000 # scale units to km
-# dat$latitude <- dat$latitude/1000
-
-# don't rely on sf because of install issues
-# # convert to UTM - kms
-# # make the UTM cols spatial (X/Easting/lon, Y/Northing/lat)
-# dat <- st_as_sf(dat, coords = c("longitude", "latitude"), crs = 4326)
-# # # transform to UTM
-# dat<- st_transform(x = dat, crs = 32610)
-# dat$longitude = st_coordinates(dat)[,1]
-# dat$latitude = st_coordinates(dat)[,2]
-# dat <- as.data.frame(dat)
-# dat$longitude <- dat$longitude / 1000
-# dat$latitude <- dat$latitude / 1000
-
 # come up with prediction grid
 resolution <- pred_resolution
 dat$floor_lon <- floor(dat$longitude / resolution)
