e02d41f340274395f6469291ce98a474e93544db
Eric Ward <eric.ward@noaa.gov>
2022-08-17 20:49:13 -0700

    add gdal
    sudo script

--- code/gen_pred_grid.R
+++ code/gen_pred_grid.R
@@ -56,3 +56,4 @@ station_df$latitude <- as.numeric(unlist(lapply(strsplit(as.character(station_df
 pred_grid <- dplyr::left_join(pred_grid, station_df)
 
 saveRDS(pred_grid, "indices/pred_grid.rds")
+
